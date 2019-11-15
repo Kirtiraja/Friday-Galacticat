7c72867683bad33d22414dc5b497f9687898fbfc
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-11-15 13:09:10 -0800

    add first test spec to test

---
+++ __tests__/calc.test.js
@@ -0,0 +1,7 @@
+import { Calc } from './../src/Calc.'
+
+  describe('Calc' , () => {
+    test('should correctly create age with user inputted age')
+    let userAge = new Calc(38);
+    expect(calc.age).toEqual(38);
+  });
--- src/Calc.js
+++ src/Calc.js
@@ -1,6 +1,17 @@
-export function Calc(userAge) {
-  let output = [];
-  // for (var i = 1; i <= userAge; i++) {
-  //   if (i % 15 === 0) {
-  //     output.push("");
-    }
+export function Calc(age) {
+  this.age = age;
+}
+
+
+
+
+//each planet's year compared to earth year.
+    // Mercury 0.241 yrs
+    // Venus 0.615 yrs
+    // Earth 1.000 yrs
+    // Mars 1.881 yrs
+    // Jupiter 11.86 yrs
+    // Saturn 29.46 yrs
+    // Uranus 84.01 yrs
+    // Neptune 164.8 yrs
+    // Pluto (dwarf planet) 248.6 yrs
